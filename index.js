876dc708c0d0c5351944df0433d1fea75b5a3fd3
Alexander Schuessler <schuessler.alex@gmail.com>
2022-04-10 10:14:06 -0500

    commit tree 9669cb3446e1f76ac9b9702f6faa7abcbc7a7ccf

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+let paraText = document.querySelector('p')
+document.addEventListener("DOMContentLoaded", function() {
+    paraText.innerHTML = 'This is really cool!'
+  });
